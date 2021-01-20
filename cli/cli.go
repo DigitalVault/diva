8613fd9a63437ff7ca853fc5c6ab4712fe6d0fb1
Michael Zimmermann <michael@ml-zn.de>
2021-01-20 22:13:57 +0100

    tests

--- cli/cli.go
+++ cli/cli.go
@@ -1,5 +1,20 @@
 package cli
 
+import (
+  "fmt"
+  "runtime/debug"
+)
+
 func Version() string {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		panic("couldn't read build info")
+	}
+
+	fmt.Printf("%s version %s\n", bi.Path, bi.Main.Version)
+
+	for _, d := range bi.Deps {
+		fmt.Printf("\tbuilt with %s version %s\n", d.Path, d.Version)
+	}
   return "v0.1"
 }
