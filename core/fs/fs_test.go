87088c358ac68dc79fa861e749eb4e43545015b9
Michael Zimmermann <michael@ml-zn.de>
2021-02-11 22:53:57 +0100

    just proceed

--- core/fs/fs_test.go
+++ core/fs/fs_test.go
@@ -2,7 +2,25 @@ package fs
 
 import (
 	"testing"
+	"strings"
+	"path/filepath"
+	"os"
+	log "github.com/sirupsen/logrus"
 )
 
 func TestInit(t *testing.T) {
+}
+
+func TestWalk(t *testing.T) {
+	root, _ := os.UserHomeDir()
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if strings.ToLower(filepath.Ext(path)) == ".pdf" {
+			log.Infof("Found PDF file : %v in %v.", filepath.Dir(path), info.Name())
+		}
+		return nil
+	})
+	if err != nil {
+		t.Error(err)
+	}
+
 }
